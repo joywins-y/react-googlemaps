06f3012f8350efc4a7902032dd386470c744f99c
windy <1519105541@qq.com>
2022-02-09 18:30:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import CurrentLocation from "./Map";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false, // Hides or shows the InfoWindow
+    activeMarker: {}, // Shows the active marker upon click
+    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+  };
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
+  render() {
+    return (
+      // <Map
+      //   google={this.props.google}
+      //   zoom={14}
+      //   style={mapStyles}
+      //   initialCenter={{
+      //     lat: -1.2884,
+      //     lng: 36.8233,
+      //   }}
+      // >
+      //   <Marker
+      //     onClick={this.onMarkerClick}
+      //     name={"Kenyatta International Convention Centre"}
+      //   />
+      //   <InfoWindow
+      //     marker={this.state.activeMarker}
+      //     visible={this.state.showingInfoWindow}
+      //     onClose={this.onClose}
+      //   >
+      //     <div>
+      //       <h4>{this.state.selectedPlace.name}</h4>
+      //     </div>
+      //   </InfoWindow>
+      // </Map>
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
+        <Marker onClick={this.onMarkerClick} name={"Current Location"} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
+    );
+  }
 }
 
-export default App;
+/**
+ * http://www.krpano360.com/tuwenxiangjieshenqinggugedituapimiyao/
+ * 需要生成google地图API 密钥
+ */
+export default GoogleApiWrapper({
+  apiKey: "YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE",
+})(MapContainer);
+// export default GoogleApiWrapper((props) => ({
+//   apiKey: props.apiKey,
+// }))(MapContainer);
